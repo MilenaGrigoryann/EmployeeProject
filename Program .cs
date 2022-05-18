d9ea01bc175e24d7fc41a20e559438f88cc4dbf6
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-05-18 19:42:58 +0400

    Add files via upload

---
+++ Program .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Employee.Interfaces;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employee = new Employee[2];
+            employee[0] = new ContractualEmployee();
+            employee[1] = new CasualEmployee();
+            for (int i = 0; i < employee.Length; i++)
+            {
+                ((IProject)employee[i]).GetProjectDetails(1555);
+
+            }
+
+        }
+    }
+}
